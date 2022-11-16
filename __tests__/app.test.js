6fcfabf9bf68a42ffaf71c971a1b1c5eb57d40c2
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-15 17:28:23 -0800

    TEST written: POST /api/v1/sessions creates a new user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,30 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'Test',
+  email: 'test@test.com',
+  password: 12345,
+};
+
+describe('users', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('POST /api/v1/sessions creates a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(mockUser);
+    expect(resp.status).toBe(200);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
   afterAll(() => {
     pool.end();

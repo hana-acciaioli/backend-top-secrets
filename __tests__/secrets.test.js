9c969a18bf6103d16c8434f78592ec52839d54cf
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-17 12:44:07 -0800

    separated tests into two suites

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -11,40 +11,9 @@ const mockUser = {
   password: '12345',
 };
 
-describe('users', () => {
+describe('secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
-  });
-  it('POST /api/v1/users creates a new user', async () => {
-    const resp = await request(app).post('/api/v1/users').send(mockUser);
-    expect(resp.status).toBe(200);
-    const { firstName, lastName, email } = mockUser;
-
-    expect(resp.body).toEqual({
-      id: expect.any(String),
-      firstName,
-      lastName,
-      email,
-    });
-  });
-  it('POST /api/v1/users/sessions logs in an existing user and creates cookie', async () => {
-    await request(app).post('/api/v1/users').send(mockUser);
-    const resp = await request(app)
-      .post('/api/v1/users/sessions')
-      .send({ email: 'jeff@defense.gov', password: '12345' });
-    expect(resp.status).toEqual(200);
-  });
-  it('DELETE /sessions deletes the user session', async () => {
-    const agent = request.agent(app);
-    // create a User directly in the database (saves an API call)
-    // const user = await UserService.create({ ...mockUser }); Removed due to user being undefined.
-    // sign in that user
-    await agent
-      .post('/api/v1/users/sessions')
-      .send({ email: 'test@example.com', password: '12345' });
-
-    const resp = await agent.delete('/api/v1/users/sessions');
-    expect(resp.status).toBe(204);
   });
   it(' /api/v1/secrets should return a 401 if not authenticated', async () => {
     const resp = await request(app).get('/api/v1/secrets');
---
+++ __tests__/users.test.js
@@ -0,0 +1,54 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService.js');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'Test',
+  email: 'jeff@defense.gov',
+  password: '12345',
+};
+
+// const createUser = await request(app).post('/api/v1/users').send(mockUser);
+
+describe('users', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('POST /api/v1/users creates a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(mockUser);
+    expect(resp.status).toBe(200);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+  it('POST /api/v1/users/sessions logs in an existing user and creates cookie', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const resp = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'jeff@defense.gov', password: '12345' });
+    expect(resp.status).toEqual(200);
+  });
+  it('DELETE /sessions deletes the user session', async () => {
+    const agent = request.agent(app);
+    // create a User directly in the database (saves an API call)
+    // const user = await UserService.create({ ...mockUser }); Removed due to user being undefined.
+    // sign in that user
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@example.com', password: '12345' });
+
+    const resp = await agent.delete('/api/v1/users/sessions');
+    expect(resp.status).toBe(204);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});

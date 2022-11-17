a614236148e563a4dc2f6b0bb3a7a20d54c948ef
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-16 17:22:00 -0800

    commented out userServices

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService.js');
+// const UserService = require('../lib/services/UserService.js');
 
 const mockUser = {
   firstName: 'Test',
